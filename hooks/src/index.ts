b7ead4cbe3d71fc8ef96e9071bd084ba321abf32
Krish Makhijani <makhijanikrish001@gmail.com>
2024-10-27 11:14:27 +0530

    Zap route modified

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -6,31 +6,35 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
-// password logic
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
     const body = req.body;
 
-    // store in db a new trigger
-    await prisma.$transaction(async tx => {
-        const run = await tx.zapRun.create({
-            data: {
-                zapId: zapId,
-                metadata: body
-            }
-        });;
-
-        await tx.zapRunOutbox.create({
-            data: {
-                zapRunId: run.id
-            }
+    try{
+        await prisma.$transaction(async tx => {
+            const run = await tx.zapRun.create({
+                data: {
+                    zapId: zapId,
+                    metadata: body
+                }
+            });;
+    
+            await tx.zapRunOutbox.create({
+                data: {
+                    zapRunId: run.id
+                }
+            })
+    
+            console.log("Zap created for user:",userId,"on zap",zapId)
         })
-    })
-    res.json({
-        message: "Webhook received"
-    })
+        res.json({
+            message: "Webhook received"
+        })
+    }catch(err){
+        console.log(err)
+    }
+    
 })
 
 app.listen(3002);
